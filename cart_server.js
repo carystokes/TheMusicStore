d50ab0baea87bf3f29ae9b43541804e7f3f1a950
carystokes <cary.p.stokes@gmail.com>
2017-04-02 14:16:00 -0400

    Add cart server

---
+++ cart_server.js
@@ -0,0 +1,58 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+function handle_incoming_request(req, res) {
+  if (/^\/[a-zA-Z0-9\/]*.css$/.test(req.url.toString())) {
+    sendFileContent(res, req.url.toString().substring(1), "text/css")
+  } else if (/^\/[a-zA-Z0-9\/]*.png$/.test(req.url.toString())) {
+    sendImage(res, req.url.toString().substring(1), "image/png")
+  } else {
+    sendPage(req, res)
+  }
+
+  function sendFileContent(res, fileName, contentType){
+    fs.readFile(fileName, function(err, data){
+      if(err){
+        res.writeHead(404);
+        res.write("File not found");
+        res.end();
+      }
+      else{
+        res.writeHead(200, {'Content-Type': contentType});
+        res.end(data);
+      }
+    });
+  }
+
+  function sendImage(res, fileName, contentType){
+    fs.readFile(fileName, function(err, img){
+      if(err){
+        res.writeHead(404);
+        res.write("File not found");
+        res.end();
+      }
+      else{
+        res.writeHead(200, {'Content-Type': contentType});
+        res.end(img, 'binary');
+      }
+    });
+  }
+
+  function sendPage(req, res) {
+    fs.readFile("./views/shopping.html", function(err, data){
+      if(err){
+        res.writeHead(404);
+        res.write("File not found");
+        res.end();
+      }
+      else{
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+      }
+    });
+  }
+}
+
+var s = http.createServer(handle_incoming_request);
+s.listen(8080);
