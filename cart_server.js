4913a652f406a7fcd8ad4185c028f8caba5fff42
carystokes <cary.p.stokes@gmail.com>
2017-04-17 22:58:53 -0400

    Add express and router

--- cart_server.js
+++ cart_server.js
@@ -1,58 +1,73 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var express = require('express');
+var index = require('./routes.index');
+var customers = require('./routes/customers');
+var orders = require('./routes/orders');
+var products = require('./routes/products');
 
-function handle_incoming_request(req, res) {
-  if (/^\/[a-zA-Z0-9\/]*.css$/.test(req.url.toString())) {
-    sendFileContent(res, req.url.toString().substring(1), "text/css")
-  } else if (/^\/[a-zA-Z0-9\/]*.png$/.test(req.url.toString())) {
-    sendImage(res, req.url.toString().substring(1), "image/png")
-  } else {
-    sendPage(req, res)
-  }
+var cart_server = express();
 
-  function sendFileContent(res, fileName, contentType){
-    fs.readFile(fileName, function(err, data){
-      if(err){
-        res.writeHead(404);
-        res.write("File not found");
-        res.end();
-      }
-      else{
-        res.writeHead(200, {'Content-Type': contentType});
-        res.end(data);
-      }
-    });
-  }
+cart_server.use('/', index);
+cart_server.use('/products', products);
+cart_server.use('/orders', orders);
+cart_server.use('/customers', customers);
 
-  function sendImage(res, fileName, contentType){
-    fs.readFile(fileName, function(err, img){
-      if(err){
-        res.writeHead(404);
-        res.write("File not found");
-        res.end();
-      }
-      else{
-        res.writeHead(200, {'Content-Type': contentType});
-        res.end(img, 'binary');
-      }
-    });
-  }
+module.exports = cart_server
 
-  function sendPage(req, res) {
-    fs.readFile("./views/shopping.html", function(err, data){
-      if(err){
-        res.writeHead(404);
-        res.write("File not found");
-        res.end();
-      }
-      else{
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(data);
-      }
-    });
-  }
-}
-
-var s = http.createServer(handle_incoming_request);
-s.listen(8080);
+// var http = require('http');
+// var fs = require('fs');
+// var url = require('url');
+//
+// function handle_incoming_request(req, res) {
+//   if (/^\/[a-zA-Z0-9\/]*.css$/.test(req.url.toString())) {
+//     sendFileContent(res, req.url.toString().substring(1), "text/css")
+//   } else if (/^\/[a-zA-Z0-9\/]*.png$/.test(req.url.toString())) {
+//     sendImage(res, req.url.toString().substring(1), "image/png")
+//   } else {
+//     sendPage(req, res)
+//   }
+//
+//   function sendFileContent(res, fileName, contentType){
+//     fs.readFile(fileName, function(err, data){
+//       if(err){
+//         res.writeHead(404);
+//         res.write("File not found");
+//         res.end();
+//       }
+//       else{
+//         res.writeHead(200, {'Content-Type': contentType});
+//         res.end(data);
+//       }
+//     });
+//   }
+//
+//   function sendImage(res, fileName, contentType){
+//     fs.readFile(fileName, function(err, img){
+//       if(err){
+//         res.writeHead(404);
+//         res.write("File not found");
+//         res.end();
+//       }
+//       else{
+//         res.writeHead(200, {'Content-Type': contentType});
+//         res.end(img, 'binary');
+//       }
+//     });
+//   }
+//
+//   function sendPage(req, res) {
+//     fs.readFile("./views/shopping.html", function(err, data){
+//       if(err){
+//         res.writeHead(404);
+//         res.write("File not found");
+//         res.end();
+//       }
+//       else{
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.end(data);
+//       }
+//     });
+//   }
+// }
+//
+// var s = http.createServer(handle_incoming_request);
+// s.listen(8080);
